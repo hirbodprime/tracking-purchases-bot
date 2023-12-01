599c43bc2848809a2458dbe149c2800c32a751d1
hirbodprime <hirbodprime@gmail.com>
2023-12-01 14:38:28 +0330

    awefawef

--- track_purchases/settings.py
+++ track_purchases/settings.py
@@ -119,7 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '../tracking_purchases/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '../tracking_purchases/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
